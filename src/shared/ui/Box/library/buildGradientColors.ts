7fe2efa78176048e805c52c998d0e225008de29c
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2025-11-16 10:28:21 -0300

    feat: add splash screen

--- src/shared/ui/Box/library/buildGradientColors.ts
+++ src/shared/ui/Box/library/buildGradientColors.ts
@@ -1,11 +1,12 @@
-import {Theme} from '@/styles';
-import {KeyColors} from '../BoxGradient';
+import { Theme } from "@/styles";
+import { KeyColors } from "../BoxGradient";
 
+type Gradient = [string, string, ...string[]];
 export function buildGradientColors(
   colors: [KeyColors, KeyColors, ...KeyColors[]],
   theme: Theme,
 ) {
-  const gradientColors: [string, string, ...string[]] = [];
+  const gradientColors: Gradient = [] as unknown as Gradient;
   for (let color of colors) {
     const hexColor = theme.colors[color];
 
